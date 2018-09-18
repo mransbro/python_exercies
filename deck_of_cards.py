d4e180c1faa60286670208d7d997d506d9113c1e
Michael Ansbro <mransbro@gmail.com>
2018-09-18 20:38:58 +0100

    Added excerise

---
+++ deck_of_cards.py
@@ -0,0 +1,36 @@
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def __repr__(self):
+        print(f"A {value} of {suit}")
+
+        suits = ("Hearts", "Diamonds", "Clubs", "Spades")
+        values = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+
+    
+class Deck:
+    def __init__(self, count)
+        self.count = count
+
+    def __repr__(self):
+        return f"Deck of {count} cards"
+
+    def count():
+        return self.count
+
+    def _deal():
+        
+
+    def shuffle():
+
+
+    def deal_card():
+        return _deal()
+        count - 1
+
+    def deal_hand(num):
+        return
+
+
