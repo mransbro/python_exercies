56efad4c80ab40374b0736018b3e8b597503cbb7
Michael Ansbro <michael.ansbro@abide-financial.com>
2018-09-19 11:08:33 +0100

    finished deck of cards

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,36 +1,50 @@
+from random import shuffle
+
+
 class Card:
-    def __init__(self, suit, value):
-        self.suit = suit
+    def __init__(self, value, suit):
         self.value = value
+        self.suit = suit
 
     def __repr__(self):
-        print(f"A {value} of {suit}")
+        return f"{self.value} of {self.suit}"
 
 
 class Deck:
-
-    suits = ("Hearts", "Diamonds", "Clubs", "Spades")
-    values = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
-
     def __init__(self):
-        self
+        suits = ("Hearts", "Diamonds", "Clubs", "Spades")
+        values = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+        self.cards = [Card(value, suit) for suit in suits for value in values]
 
     def __repr__(self):
-        return f"Deck of {count} cards"
+        return f"Deck of {self.count()} cards"
 
-    def count():
-        return self.count
+    def count(self):
+        return len(self.cards)
 
-    def _deal():
-        return
+    def _deal(self, num):
+        count = self.count()
+        actual = min([count, num])
+        print(f"Going to remove {actual} cards")
+        if count == 0:
+            raise ValueError("All cards have been dealt")
+        cards = self.cards[-actual:]
+        self.cards = self.cards[:-actual]
+        return cards
 
-    def shuffle():
-        return
+    def deal_card(self):
+        return self._deal(1)[0]
 
-    def deal_card():
-        return _deal()
-        count - 1
+    def deal_hand(self, hand_size):
+        return self._deal(hand_size)
 
-    def deal_hand(num):
-        return
+    def shuffle(self):
+        if self.count() < 52:
+            raise ValueError("Only full decks can be shuffled")
+        shuffle(self.cards)
+        return self
 
+
+d = Deck()
+print(d._deal(3))
+print(d.count())
